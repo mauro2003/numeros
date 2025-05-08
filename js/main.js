80ac3d3a95884d8300464a082d66d339c71a96ef
mauro2003 <76508668+mauro2003@users.noreply.github.com>
2025-05-08 17:05:48 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-// js/main.js
+/// js/main.js
+
 import { iniciarJuego, verificar, reiniciar, cambiarJugador } from './juego.js';
 import { cargarRanking } from './ranking.js';
-import { inicializarModoOscuro } from './ui.js';
+import { inicializarModoOscuro, inicializarAudio } from './ui.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   // Eventos del juego
-  document.getElementById('dificultad').addEventListener('change', iniciarJuego);
-  document.getElementById('btnAdivinar').addEventListener('click', verificar);
-  document.getElementById('btnReiniciar').addEventListener('click', reiniciar);
-  document.getElementById('btnCambiar').addEventListener('click', cambiarJugador);
+  document.getElementById('dificultad')
+          .addEventListener('change', iniciarJuego);
+  document.getElementById('btnAdivinar')
+          .addEventListener('click', verificar);
+  document.getElementById('btnReiniciar')
+          .addEventListener('click', reiniciar);
+  document.getElementById('btnCambiar')
+          .addEventListener('click', cambiarJugador);
 
-  // Carga inicial del ranking y modo oscuro
+  // Carga inicial del ranking, modo oscuro y audio
   cargarRanking();
   inicializarModoOscuro();
+  inicializarAudio();
 });
